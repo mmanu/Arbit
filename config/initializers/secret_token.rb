dff1f0065739c841e042f4dbb4355dc00c56b7e8
chaturvedi <chaturvedi@chaturvedi-laptop.(none)>
2011-02-12 17:07:50 +0530

    Shri Ganeshaaye Namah

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arbit::Application.config.secret_token = '325057117930b96ad91c3c81762f98b1749c38c72e3cf61eb605da0512431f9cb1aabdb83b195b566fda6a327dc3d0c122de70456cd49d15beb7d32484c12c9c'
